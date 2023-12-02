2de84fba8623cd0a6711ef59e06a45535a48e22d
methmenghour <methmenghour@gmail.com>
2023-12-02 18:32:34 +0700

    first set up project

---
+++ src/main/java/com/menghour/java/school/phoneshopnight/PhoneshopNightApplication.java
@@ -0,0 +1,13 @@
+package com.menghour.java.school.phoneshopnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneshopNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneshopNightApplication.class, args);
+	}
+
+}
